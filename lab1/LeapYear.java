b17b2cae90644443a45e1f1c298b3b8ae4442ad1
李家卓 <1623304384@qq.com>
2022-11-05 12:01:03 +0800

    Update

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -18,6 +18,7 @@ public class LeapYear {
         }
     }
 
+    
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
